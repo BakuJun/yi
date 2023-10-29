0f185efc985a96f5e40cbc469e396294a085e531
baku <847743331@qq.com>
2023-10-29 17:02:37 +0800

    feat: 使用HashRouter 其他页面可以直接访问

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'mobx-react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import store from './store';
 import App from './App'
 import './reset.css'
@@ -10,9 +10,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider {...store}>
         <div className='app'>
-            <BrowserRouter>
+            <HashRouter>
                 <App />
-            </BrowserRouter>
+            </HashRouter>
         </div>
     </Provider>
 )

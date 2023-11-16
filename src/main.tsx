7a8b2bdcef00ba445fd3f5834dbc1e35c4b086eb
baku <847743331@qq.com>
2023-11-16 21:33:42 +0800

    feat: 备案和favicon

--- src/main.tsx
+++ src/main.tsx
@@ -14,5 +14,10 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
                 <App />
             </HashRouter>
         </div>
+        <div className="page-footer flex-center">
+            <a className="icp" href="https://beian.miit.gov.cn/" target="_blank">
+                浙ICP备2023039790号-1
+            </a>
+        </div>
     </Provider>
 )
